dec703583cc108eb21c430b172417de896edf584
Anthonypicado <anthony_p_z@hotmail.com>
2022-08-30 02:57:14 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+/* funcion que se ejecuta al cargar la pagina */
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuseText").innerHTML = generarExcusas();
+};
+let generarExcusas = () => {
+  let pronoun = ["Un/a", "La/El"];
+  let subject = [
+    "mosca 🦟",
+    "gato 🐱",
+    "perro 🐶",
+    "payaso 🤡",
+    "abuelita 🤶",
+    "Mickey 🐭"
+  ];
+  let action = ["tomo mi", "tiró mi", "le grito a mi", "robo mi", "mordio mi"];
+  let possetion = [
+    "tarea 📖",
+    "auto 🚗",
+    "bicicleta 🚲",
+    "zapato 👞",
+    "dedo del pie 🦶"
+  ];
+  let where = ["en la calle.", "en mi casa. 🏠", "en la entrada del colegio."];
+  /* generando un numero aleatorio entr el largo de la aray */
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subjIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndex] +
+    " " +
+    subject[subjIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
